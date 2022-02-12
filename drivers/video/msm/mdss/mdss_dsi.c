a0e1336cd2b530b28bea6b3f85618dd8a91074c9
DeepakChaurasia30 <chaurasia.deepak.300@gmail.com>
2022-02-12 15:16:27 +0530

    mdss:Remove condition check and add debug line *Add pr_info to check if lcd is oon of off to pass input current according to it *Thanks @abhishekt295 for debbug code and help alot

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -33,11 +33,8 @@
 #include "mdss_debug.h"
 #include "mdss_livedisplay.h"
 
-#ifdef CONFIG_MACH_OPPO
-#include <soc/oppo/oppo_project.h>
 extern void opchg_check_lcd_on(void);
 extern void opchg_check_lcd_off(void);
-#endif
 
 #define XO_CLK_RATE	19200000
 #ifdef CONFIG_MACH_WT86518
@@ -511,13 +508,11 @@ static int mdss_dsi_off(struct mdss_panel_data *pdata, int power_state)
 		return -EINVAL;
 	}
 
-#ifdef CONFIG_MACH_OPPO
-/*Add for pass screen off current value*/
-	if (is_project(OPPO_15399)) 
-{
+        {
 	opchg_check_lcd_off();
+	pr_info("opchg_check_lcd_off executed ");
 	}
-#endif
+
 	ctrl_pdata = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 
@@ -607,13 +602,11 @@ int mdss_dsi_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
-#ifdef CONFIG_MACH_OPPO
-/*Add for pass screen on current value*/
-	if (is_project(OPPO_15399)) 
-{
+        { 
 	opchg_check_lcd_on();
+	pr_info("opchg_check_lcd_on executed ");
 	}
-#endif
+
 	ctrl_pdata = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 
