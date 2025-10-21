7033a50dea34575c889e106a99e844cbd08af5b5
nhansp <nhan.094.vn@gmail.com>
2025-10-22 04:47:17 +0700

    Revert "USB: phy: Disable phy_irq before processing LPM sequence"
    This reverts commit 4b5040e190d870096bfe8a8a8a46aa925c8a46f8.

--- drivers/usb/phy/phy-msm-usb.c
+++ drivers/usb/phy/phy-msm-usb.c
@@ -1320,8 +1320,6 @@ static int msm_otg_suspend(struct msm_otg *motg)
 
 	motg->ui_enabled = 0;
 	disable_irq(motg->irq);
-	if (motg->phy_irq)
-		disable_irq(motg->phy_irq);
 lpm_start:
 	host_bus_suspend = !test_bit(MHL, &motg->inputs) && phy->otg->host &&
 		!test_bit(ID, &motg->inputs);
@@ -1363,8 +1361,6 @@ lpm_start:
 			motg->pm_done = 1;
 		motg->ui_enabled = 1;
 		enable_irq(motg->irq);
-		if (motg->phy_irq)
-			enable_irq(motg->phy_irq);
 		return -EBUSY;
 	}
 
@@ -1632,8 +1628,6 @@ phcd_retry:
 phy_suspend_fail:
 	motg->ui_enabled = 1;
 	enable_irq(motg->irq);
-	if (motg->phy_irq)
-		enable_irq(motg->phy_irq);
 	return ret;
 }
 
